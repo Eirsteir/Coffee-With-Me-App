a78584e87fb69e1190676088130f725474302d87
Eirik Steira <eirsteir@stud.ntnu.no>
2021-06-04 13:23:38 +0200

    fix(auth): auth navigation to react navigation v5

--- src/nav/AuthNavigator.js
+++ src/nav/AuthNavigator.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../screens/SignInScreen'
@@ -11,18 +12,18 @@ const navigationOptions = {
 
 const Stack = createStackNavigator();
 
-class AuthNavigator extends React.Component {
-  render() {
-    return (
+const AuthNavigator = () => {
+  return (
+    <NavigationContainer>
       <Stack.Navigator 
         screenOptions={navigationOptions}
       >
-        <Stack.Screen component={SignIn}></Stack.Screen>
-        <Stack.Screen component={SignUp}></Stack.Screen>
-        <Stack.Screen component={ForgotPassword}></Stack.Screen>
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       </Stack.Navigator>
-    )
-  }
+    </ NavigationContainer>
+  )
 }
 
-export default AuthNavigator
+export default AuthNavigator;
