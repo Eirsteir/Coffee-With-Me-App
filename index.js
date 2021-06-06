18dc4660e4a401f94a3b8314088970f7902ef7bb
Eirik Steira <eirsteir@stud.ntnu.no>
2021-06-06 22:45:02 +0200

    chore(dependencies): clean up and upgrade dependencies

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 import { registerRootComponent} from 'expo';
-import {activateKeepAwake} from 'expo-keep-awake'
-import DEVELOPMENT from './src/constants/Constants';
 import App from './src/App';
-
-import {
-  ENV,
-  STREAM_API_KEY,
-  STREAM_API_SECRET,
-} from 'babel-dotenv';
-
-
-if (ENV == DEVELOPMENT) {
-  activateKeepAwake();
-
-  const stream = require('getstream'); 
-  // instantiate a new client (server side) 
-  const client = stream.connect( 
-    STREAM_API_KEY, 
-    STREAM_API_SECRET
-  );
-   
-  // disable auth checks, allows dev token usage 
-  client.updateAppSettings({ 
-    disable_auth_checks: true, 
-  }); 
-}
 
 registerRootComponent(App);
